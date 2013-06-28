be9d2bba307bf26fc9702479db1602dd097abafd
José Padilla <jpadilla@webapplicate.com>
2013-06-28 15:05:06 -0400

    Add users controller and index view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+    # GET /users
+    # GET /users.json
+    def index
+      @users = User.all
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SoprPlatform::Application.routes.draw do
-  resources :startups
-
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+
+  devise_for :users
+
+  resources :startups
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
