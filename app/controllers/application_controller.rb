3bd3ec10b19df317dcb52dc15b236e6b3a9e60b3
José Padilla <jpadilla@webapplicate.com>
2013-06-30 18:53:43 -0400

    Permit all fields for account_update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:full_name, :username, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) {
-        |u| u.permit(:full_name, :username, :email, :password, :password_confirmation, :current_password)
-    }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit! }
   end
 end
