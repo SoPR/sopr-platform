d3129df1dc044a99ea4600d32615b184116e5bcf
r23712 <ryan.n.oliver@gmail.com>
2013-07-02 19:35:32 -0400

    user cards

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 50})
+  def gravatar_for(user, options = { size: 140})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     return "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
