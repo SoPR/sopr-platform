8f8cc47580ba6eb07d26c6b93dcd5ebcca5708e6
José Padilla <jpadilla@webapplicate.com>
2013-07-02 11:52:37 -0400

    WIP: People page
    Includes:
    - Compass
    - Icons/Sprite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
