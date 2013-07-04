00795442df42c912dde0e829b5dd4476f4b33f63
José Padilla <jpadilla@webapplicate.com>
2013-07-04 15:25:20 -0400

    Remove compass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ SoprPlatform::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
