9d1203b490389a986bb766d59d47b2a6db2e34e0
José Padilla <jpadilla@webapplicate.com>
2013-07-06 08:57:21 -0400

    Validate length of user bio

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   attr_accessor :login
   validates_presence_of :username, :full_name
   validates_uniqueness_of :username
+  validates_length_of :bio, :maximum => 120
 
   def full_name
     if first_name and last_name
