94753e3eb82d9e9e0209368ac770d460cfac8c74
José Padilla <jpadilla@webapplicate.com>
2013-07-07 21:39:39 -0400

    Better flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
+
   def body_class
     [controller_name, action_name].join('-')
   end
+
+  def bootstrap_class_for flash_type
+    {
+        success: 'alert-success',
+        error: 'alert-error',
+        alert: 'alert-block',
+        notice: 'alert-info'
+    }[flash_type] || flash_type.to_s
+  end
+
 end
