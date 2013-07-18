5b41087a47c79ebdc1bfec3f6445ed73fbdfb26b
Jonah Ruiz <jonah@pixelhipsters.com>
2013-07-18 09:45:21 -0400

    Add startup helper methods for getting names and generating markup

--- app/helpers/startups_helper.rb
+++ app/helpers/startups_helper.rb
@@ -1,2 +1,11 @@
 module StartupsHelper
+  def market_names(markets)
+    markets.map { |market| market.name }
+  end
+
+  def market_names_list(markets)
+    market_names(markets).each do |market|
+      concat content_tag(:li, market, class: 'markets')
+    end
+  end
 end
