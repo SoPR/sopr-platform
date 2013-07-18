468189259bc33bddee30cc64b661c122730948da
Jonah Ruiz <jonah@pixelhipsters.com>
2013-07-18 07:54:51 -0400

    Add helper methods for generating social icon links

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,25 +1,78 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 140})
+  def gravatar_for(user, options = { size: 140 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    return "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
+
   def facebook_url(user)
-    return "https://facebook.com/#{user.facebook_username}"
+    "https://facebook.com/#{user.facebook_username}"
   end
+
   def twitter_url(user)
-    return "https://twitter.com/#{user.twitter_username}"
+    "https://twitter.com/#{user.twitter_username}"
   end
+
   def linkedin_url(user)
-    return "http://www.linkedin.com/in/#{user.linkedin_username}"
+    "http://www.linkedin.com/in/#{user.linkedin_username}"
   end
+
   def github_url(user)
-    return "https://github.com/#{user.github_username}"
+    "https://github.com/#{user.github_username}"
   end
+
   def flickr_url(user)
-    return "http://www.flickr.com/people/#{user.flickr_username}/"
+    "http://www.flickr.com/people/#{user.flickr_username}/"
   end
+
   def dribbble_url(user)
-    return "http://dribbble.com/#{user.dribbble_username}"
+    "http://dribbble.com/#{user.dribbble_username}"
+  end
+
+  def social_icon_links(user)
+    content_tag :ul do
+      concat(content_tag(:li, facebook_icon_link(user)) ) if user.facebook_username?
+      concat(content_tag(:li, twitter_icon_link(user))) if user.twitter_username?
+      concat(content_tag(:li, linkedin_icon_link(user))) if user.linkedin_username?
+      concat(content_tag(:li, github_icon_link(user))) if user.github_username?
+      concat(content_tag(:li, flickr_icon_link(user))) if user.flickr_username?
+      concat(content_tag(:li, dribbble_icon_link(user))) if user.dribbble_username?
+    end
+  end
+
+  def facebook_icon_link(user)
+    link_to facebook_url(user) do
+      content_tag(:i, '', class: 'icon-facebook-1')
+    end
+  end
+
+  def twitter_icon_link(user)
+    link_to twitter_url(user) do
+      content_tag(:i, '', class: 'icon-twitter-1')
+    end
+  end
+
+  def linkedin_icon_link(user)
+    link_to linkedin_url(user) do
+      content_tag(:i, '', class: 'icon-linkedin-2')
+    end
+  end
+
+  def github_icon_link(user)
+    link_to github_url(user) do
+      content_tag(:i, '', class: 'icon-github')
+    end
+  end
+
+  def flickr_icon_link(user)
+    link_to flickr_url(user) do
+      content_tag(:i, '', class: 'icon-flickr')
+    end
+  end
+
+  def dribbble_icon_link(user)
+    link_to dribbble_url(user) do
+      content_tag(:i, '', class: 'icon-dribbble-1')
+    end
   end
 end
