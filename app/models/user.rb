ffac40d428e12babe87dd081c0b23988d797b289
Jonah Ruiz <jonah@pixelhipsters.com>
2013-07-19 03:40:49 -0400

    Add include Gravatar module to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :startups
+
+  include Gravatar
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
