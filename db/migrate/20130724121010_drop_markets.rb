fd7e97df33f9c4b2a73e1e7b5f7935e3e2154a99
José Padilla <jpadilla@webapplicate.com>
2013-07-24 08:36:40 -0400

    Fix: check if table exists before dropping

--- db/migrate/20130724121010_drop_markets.rb
+++ db/migrate/20130724121010_drop_markets.rb
@@ -1,6 +1,10 @@
 class DropMarkets < ActiveRecord::Migration
   def change
-    drop_table :markets
-    drop_table :markets_startups
+    if ActiveRecord::Base.connection.table_exists? 'markets'
+      drop_table :markets
+    end
+    if ActiveRecord::Base.connection.table_exists? 'markets_startups'
+      drop_table :markets_startups
+    end
   end
 end
