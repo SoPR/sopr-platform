971bd1ef9607dca290e12c65e5a3f49d3d73a8b3
Jonah Ruiz <jonah@pixelhipsters.com>
2013-07-26 01:03:28 -0400

    Add UserMailer and Welcome email views

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: ENV['SMTP_USERNAME']
+
+  def welcome_email(user)
+    @user = user
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
+    mail(to: @user.email, subject: 'Welcome to Startups of Puerto Rico')
+  end
+end
