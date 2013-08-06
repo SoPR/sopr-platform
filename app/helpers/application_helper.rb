49d8fe93d9331d6697ecac8a937d528d9662e3b8
r23712 <ryan.n.oliver@gmail.com>
2013-08-06 19:52:21 -0400

    home page, startup forms, minor changes in style and colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,16 @@ module ApplicationHelper
     }[flash_type] || flash_type.to_s
   end
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
