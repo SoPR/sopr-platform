0d6ac1bf2a452f14ea233696ab9cd96db608698d
r23712 <ryan.n.oliver@gmail.com>
2013-09-19 19:09:10 -0400

    event ui and fix event datetime selector

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
+    @event_months = @events.group_by { |t| t.created_at.month }
   end
 
   # GET /events/1
