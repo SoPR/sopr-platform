ca4ace09a65e81fb9f2fbf5e7a898f7ee619cc9e
r23712 <ryan.n.oliver@gmail.com>
2013-09-19 16:10:30 -0400

    add a new uploader size for event

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -38,6 +38,10 @@ class ImageUploader < CarrierWave::Uploader::Base
      process :resize_to_limit => [100, 100]
    end
 
+   version :normal do
+     process :resize_to_limit => [543, 190]
+   end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
