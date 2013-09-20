22ae657d7d1c03d2beb8e85b1882329b95c44ecf
Ryan Oliver <ryan.n.oliver@gmail.com>
2013-09-20 11:25:23 -0700

    Delete blimp api

--- config/initializers/blimp_api.rb
+++
@@ -1,6 +0,0 @@
-# Blimp API Integration
-
-Blimp.api_key = "37cfb7c044ffce4b23c37b24a7b11a2b985d08ea"
-Blimp.app_id = "47f0d224976e85c6af31bb997c8238f04893db06"
-Blimp.app_secret = "8b7523b90dabca1555137506c68adb5f9534faed"
-Blimp.username = "ryan.oliver"
