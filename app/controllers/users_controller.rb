0d64e47509967298b7d229b33da9821c5e747324
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-21 16:39:52 -0400

    Refactor user instance_variable assignment to the result of conditional in its controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
     # If no search_query is provided then, return all users as usual.
-    if params[:search_query].nil? or params[:search_query].blank?
-      @users = User.order("created_at").page params[:page]
+    @users = if params[:search_query].nil? || params[:search_query].blank?
+      User.order("created_at").page params[:page]
     else
-      @users = User.search(String.new(params[:search_query])).page params[:page]
+      User.search(String.new(params[:search_query])).page params[:page]
     end
   end
 end
