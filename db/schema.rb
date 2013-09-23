0ed4d06d0c6b532e70456f1982d248c7f5f09c2f
crazirican <fernanlink@gmail.com>
2013-09-22 23:59:26 -0400

    Updated README instructions for setting up PostgreSQL on dev. Changed database.yml database configuration to use PostgreSQL on dev.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921142710) do
+ActiveRecord::Schema.define(version: 20130921141716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
