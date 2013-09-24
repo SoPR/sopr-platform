81bed8feba07d369fbd8c20d85629937b84a731b
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-24 15:59:29 -0400

    Update events factory with correct sequence

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :event do
-    sequence(:name) { |e| "After Me" }
-    sequence(:place) { |e| "Blimpie" }
+    sequence(:name) { |e| "After Me #{e.next}" }
+    sequence(:place) { |e| "Blimpie #{e.next}" }
+    date Time.now
   end
 end
