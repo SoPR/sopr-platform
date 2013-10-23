73608f7a4f0f58894489691dee686957205cc9fd
r23712 <ryan.n.oliver@gmail.com>
2013-10-23 11:46:28 -0400

    image uploader and public activity

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
+
+  # Public Activity
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) { controller && controller.current_user }
 end
