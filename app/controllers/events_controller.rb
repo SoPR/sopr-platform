36b13bb995a98d11a97e9784b10942d1f8d78c8f
r23712 <ryan.n.oliver@gmail.com>
2013-10-23 11:55:46 -0400

    events created by user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,7 @@ class EventsController < ApplicationController
   # POST /events.json
   def create
     @event = Event.new(event_params)
+    @event.user = current_user
 
     respond_to do |format|
       if @event.save
