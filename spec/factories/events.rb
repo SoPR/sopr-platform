5dff39824a6209604da0342ca7cbe9b3855fe373
r23712 <ryan.n.oliver@gmail.com>
2013-10-23 14:30:47 -0400

    factory girl test

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,11 +2,8 @@
 
 FactoryGirl.define do
   factory :event do
-    name "MyString"
-    place "MyString"
-    date "2013-10-23 11:44:15"
-    image "MyString"
-    url "MyString"
-    user nil
+    sequence(:name) { |e| "After Me #{e.next}" }
+    sequence(:place) { |e| "Blimpie #{e.next}" }
+    date Time.now
   end
 end
