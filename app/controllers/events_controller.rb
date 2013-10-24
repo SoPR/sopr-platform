9695078ebe6f911f97bc19c5a4d08a5d8edac067
José Padilla <jpadilla@webapplicate.com>
2013-10-24 00:09:10 -0400

    Order and filter events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.where(['date >= ?', DateTime.now]).order('date')
 
     respond_to do |format|
       format.html
